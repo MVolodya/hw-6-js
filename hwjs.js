95d7e3c4b8b7f55e1df474ed1da2698027f802a8
MVolodya <medvid.volodya@gmail.com>
2017-08-30 15:42:30 +0300

    added two files & task1 with prompt

---
+++ hwjs.js
@@ -0,0 +1,6 @@
+var person = "Volodymyr Medvid";
+var time = "Mon Sep 25 2017 12:00";
+var place = "Lviv";
+var event = prompt("Do you have a some meeting?");
+alert(person + " have a meeting " + event + " in " + place + " on " + time);
+console.log(person + " have a meeting " + event + " in " + place + " on " + time);
