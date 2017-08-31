b720c90429ed99fe4f6b88457d8a9db7c934b293
MVolodya <medvid.volodya@gmail.com>
2017-08-31 05:12:15 +0300

    added basic functional to js file

--- hwjs.js
+++ hwjs.js
@@ -6,25 +6,34 @@ var event = prompt("Do you have a some meeting?");
 console.log(resultPerson());
 alert(resultPerson());
 
-const USD = 25;
-const EUR = 30;
-const EUR_USD = 1.2;
-
-var usd = prompt("USD");
-var eur = prompt("EUR");
-
-console.log(resultMoney());
-alert(resultMoney());
-
 function resultPerson() {
     return (person + " have a meeting " + event + " in " + place + " on " + time);
 }
 
-function resultMoney() {
-    return (eur + " euros are equal " + eur * EUR + " UAH, "+
-    usd + " dollars are equal " + usd * USD + " UAH, " +
-    "one euro is equal " + EUR_USD + " dollars.");
+const USD = 25;
+const EUR = 30;
+const EUR_USD = 1.2;
+
+var eur = prompt("EUR to UAH");
+var usd = prompt("USD to UAH");
+var eur_usd = prompt("EUR to USD");
+
+var eurConverted = currencyConverter(eur,EUR);
+var usdConverted = currencyConverter(usd,USD);
+var eur_usdConverted = currencyConverter(eur_usd,EUR_USD);
+
+console.log(resultConcatenation());
+alert(resultConcatenation());
+
+function resultConcatenation(){
+    return (eur + " euros are equal " + eurConverted + " UAH, "+
+            usd + " dollars are equal " + usdConverted + " UAH, " +
+            eur_usd + " euros are equal " + eur_usdConverted + " USD");
+}
+
+function currencyConverter(money, currency){
+    return money * currency;
 }
 
 document.getElementById('task1').innerHTML=resultPerson();
-document.getElementById('task2').innerHTML=resultMoney();
+document.getElementById('task2').innerHTML=resultConcatenation();
