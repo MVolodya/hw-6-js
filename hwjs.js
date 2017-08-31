e6751c380d085f62d11f8438bc7a23edd733658f
MVolodya <medvid.volodya@gmail.com>
2017-08-31 20:52:39 +0300

    function for checking prompt if empty

--- hwjs.js
+++ hwjs.js
@@ -1,29 +1,46 @@
-var person = "Volodymyr Medvid";
+var person = prompt("What's your name", "Volodymyr Medvid");
 var time = "Mon Sep 25 2017 12:00";
 var place = "Lviv";
-var event = prompt("Do you have a some meeting?");
+var personEvent = prompt("Do you have a some meeting?", "meet my friend");
 
 console.log(resultPerson());
 alert(resultPerson());
 
 function resultPerson() {
-    return (person + " have a meeting " + event + " in " + place + " on " + time);
+    return (person + " have a meeting " + personEvent + " in " + place + " on " + time);
 }
 
 const USD = 25;
 const EUR = 30;
 const EUR_USD = 1.2;
+const EUR_to_UAH = "EUR to UAH";
+const USD_to_UAH = "USD to UAH";
+const EUR_to_USD = "EUR to USD";
 
-var eur = prompt("EUR to UAH");
-var usd = prompt("USD to UAH");
-var eur_usd = prompt("EUR to USD");
+var eur = prompt(EUR_to_UAH, 1);
+eur = checkPromptIsEmpty(eur, EUR_to_UAH);
+
+var usd = prompt(USD_to_UAH, 1);
+usd = checkPromptIsEmpty(usd, USD_to_UAH);
+
+var eur_usd = prompt(EUR_to_USD, 1);
+eur_usd = checkPromptIsEmpty(eur_usd, EUR_to_USD);
 
 var eurConverted = currencyConverter(eur,EUR);
 var usdConverted = currencyConverter(usd,USD);
 var eur_usdConverted = currencyConverter(eur_usd,EUR_USD);
 
-console.log(resultConcatenation());
-alert(resultConcatenation());
+
+function checkPromptIsEmpty(money, message){
+    while (money === "" || money == null) {
+        money  = prompt("Oops, try again " + message, 1);
+    }
+    return money;
+}
+
+function currencyConverter(money, currency){
+    return money * currency;
+}
 
 function resultConcatenation(){
     return (eur + " euros are equal " + eurConverted + " UAH, "+
@@ -31,9 +48,8 @@ function resultConcatenation(){
             eur_usd + " euros are equal " + eur_usdConverted + " USD");
 }
 
-function currencyConverter(money, currency){
-    return money * currency;
-}
+console.log(resultConcatenation());
+alert(resultConcatenation());
 
 document.getElementById('task1').innerHTML=resultPerson();
 document.getElementById('task2').innerHTML=resultConcatenation();
